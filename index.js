23cf10e0c4c2f371a9428ae8faea8b870a3bbbeb
Harold Torres Marino <haroldtm55@gmail.com>
2021-05-04 16:20:44 +1000

    Passed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+/*function addingEventListener() {
+  const input2 = document.querySelectorAll('div')[5];
+  input2.addEventListener('click', function() {
+    alert('I was clicked');
+  });
+};*/
+
 function addingEventListener() {
-}
+  const input = document.getElementById('input');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
+};
